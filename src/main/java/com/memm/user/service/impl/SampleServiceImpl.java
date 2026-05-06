be07dbc571b5fb8c5019da30c6694b4e00227183
memiik <memiik@protonmail.com>
2026-05-06 16:32:07 +0300

    delete sample user and update service layer

--- src/main/java/com/memm/user/service/impl/SampleServiceImpl.java
+++ src/main/java/com/memm/user/service/impl/SampleServiceImpl.java
@@ -1,14 +1,6 @@
 package com.memm.user.service.impl;
 
-import com.memm.user.model.SampleUser;
 import com.memm.user.service.SampleService;
 
-import java.util.UUID;
-
 public class SampleServiceImpl implements SampleService {
-
-    @Override
-    public SampleUser findById(UUID id) {
-        return null;
-    }
 }
