503fa8ce944390d44a55981d67b1a1a02d103dac
memiik <memiik@protonmail.com>
2026-05-06 16:30:40 +0300

    create BaseModel

---
+++ src/main/java/com/memm/user/model/BaseModel.java
@@ -0,0 +1,39 @@
+package com.memm.user.model;
+
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public abstract class BaseModel {
+
+    @PrimaryKey
+    private UUID id;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
